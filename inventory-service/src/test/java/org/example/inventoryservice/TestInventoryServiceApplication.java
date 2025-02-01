cd2f293e1df13a732d11e04bbaec3537b0ace548
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-01 14:39:53 +0100

    initial version for inventory service

---
+++ inventory-service/src/test/java/org/example/inventoryservice/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package org.example.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
