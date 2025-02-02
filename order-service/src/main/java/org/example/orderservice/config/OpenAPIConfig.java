01dea774fac97af4d95cc6f9a267322d2ea32d34
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-02 20:59:48 +0100

    added swagger in order service

---
+++ order-service/src/main/java/org/example/orderservice/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package org.example.orderservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class OpenAPIConfig {
+
+    @Bean
+    OpenAPI orderServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Order Service API")
+                        .description("This is the REST API for order service")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0")));
+    }
+}
