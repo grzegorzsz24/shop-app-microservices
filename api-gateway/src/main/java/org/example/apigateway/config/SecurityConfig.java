adfae9f7d86a04e2755e7c94e8b2f793263691c9
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-02 16:02:06 +0100

    added keycloak

---
+++ api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package org.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+                .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
