f77d7e778632146ca3b365d6b209bcd756779e52
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-04 14:01:30 +0100

    moved from OpenFeign to the RestClient

---
+++ order-service/src/main/java/org/example/orderservice/config/RestClientConfig.java
@@ -0,0 +1,26 @@
+package org.example.orderservice.config;
+
+import org.example.orderservice.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+}
