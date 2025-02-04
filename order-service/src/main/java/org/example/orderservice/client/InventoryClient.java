e0d9305b015cfcddf6d7f080c9f8228aeaf2e4b8
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-04 17:12:58 +0100

    feat: added circuit breaker to the rest client in order service

--- order-service/src/main/java/org/example/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/org/example/orderservice/client/InventoryClient.java
@@ -1,10 +1,19 @@
 package org.example.orderservice.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
 
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        // TODO add logging
+        return false;
+    }
 }
