28b107cc856be465e9ec3ddac476217c2aa4b37c
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-10 12:35:44 +0100

    feat: redis config

---
+++ cart-service/src/main/java/org/example/cartservice/config/RedisConfig.java
@@ -0,0 +1,61 @@
+package org.example.cartservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+class RedisConfig {
+
+    @Value("${spring.redis.data.host}")
+    private String host;
+
+    @Value("${spring.redis.data.port}")
+    private int port;
+
+    @Value("${spring.redis.data.password}")
+    private String password;
+
+    @Bean
+    RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(password);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+    }
+
+    @Bean
+    RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cartCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(1))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        cacheConfigurations.put("deletionTokens", cartCacheConfig);
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(this.cacheConfiguration())
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+    }
+}
