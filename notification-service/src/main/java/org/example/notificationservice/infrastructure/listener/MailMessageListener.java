76bc004596f2a933616958a508901f62b69a768e
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-14 16:38:23 +0100

    feat: failed message retry

--- notification-service/src/main/java/org/example/notificationservice/infrastructure/listener/MailMessageListener.java
+++ notification-service/src/main/java/org/example/notificationservice/infrastructure/listener/MailMessageListener.java
@@ -2,8 +2,10 @@ package org.example.notificationservice.infrastructure.listener;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.notificationservice.domain.model.Mail;
 import org.example.notificationservice.domain.service.MailSenderService;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 class MailMessageListener {
     private final MailSenderService mailService;
     private final RabbitTemplate rabbitTemplate;
@@ -24,5 +27,24 @@ class MailMessageListener {
     @RabbitListener(queues = "${app.rabbitmq.queues.mail}")
     void processMessage(@Valid Mail mail) {
         mailService.sendMail(mail);
+    }
+
+    @RabbitListener(queues = "${app.rabbitmq.queues.mail-dead-letter}")
+    void processFailedMessagesRetryHeaders(Message failedMessage) {
+        Integer retriesCnt = (Integer) failedMessage.getMessageProperties()
+                .getHeaders().get(HEADER_X_RETRIES_COUNT);
+
+        if (retriesCnt == null) retriesCnt = 1;
+        if (retriesCnt > MAX_RETRIES_COUNT) {
+            log.info("Discarding failed message with retries count {}", retriesCnt);
+            return;
+        }
+
+        log.info("Retrying message for the {} time", retriesCnt);
+        failedMessage.getMessageProperties()
+                .getHeaders().put(HEADER_X_RETRIES_COUNT, ++retriesCnt);
+
+        rabbitTemplate.send(mailExchange,
+                failedMessage.getMessageProperties().getReceivedRoutingKey(), failedMessage);
     }
 }
