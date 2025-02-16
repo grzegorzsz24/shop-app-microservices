ea7fb89d26630358eb3c6f29abc2d249260600d5
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-16 16:20:15 +0100

    feat: rabbitmq config

---
+++ order-service/src/main/java/org/example/orderservice/infrastructure/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package org.example.orderservice.infrastructure.config;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class RabbitMQConfig {
+
+    @Value("${app.rabbitmq.exchanges.mails}")
+    private String exchangeMail;
+
+    @Bean
+    AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    DirectExchange messagesExchange() {
+        return new DirectExchange(exchangeMail);
+    }
+
+    @Bean
+    Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
