ff248289d6d3934e5b7b6b1f733691863db6d28f
Grzegorz <grzesio.szymanek@gmail.com>
2025-02-25 16:22:48 +0100

    fix: recover unnecessary payment response constructor

--- order-service/src/main/java/org/example/orderservice/application/dto/payment/PaymentStatusResponse.java
+++ order-service/src/main/java/org/example/orderservice/application/dto/payment/PaymentStatusResponse.java
@@ -6,4 +6,8 @@ public record PaymentStatusResponse(
         String paymentSessionId,
         PaymentStatus paymentStatus
 ) {
+    @SuppressWarnings("unused")
+    public PaymentStatusResponse(String paymentSessionId, String paymentStatus) {
+        this(paymentSessionId, PaymentStatus.valueOf(paymentStatus));
+    }
 }
